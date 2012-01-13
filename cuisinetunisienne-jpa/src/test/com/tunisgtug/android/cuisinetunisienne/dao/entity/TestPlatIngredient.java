29b4f2c5d651ec43259af98392e4e92cd5a61aa4
jed3g09 <jed3g09@gmail.com>
2012-01-13 20:21:34 +0000

    Organize imports

--- cuisinetunisienne-jpa/src/test/com/tunisgtug/android/cuisinetunisienne/dao/entity/TestPlatIngredient.java
+++ cuisinetunisienne-jpa/src/test/com/tunisgtug/android/cuisinetunisienne/dao/entity/TestPlatIngredient.java
@@ -3,20 +3,11 @@ package test.com.tunisgtug.android.cuisinetunisienne.dao.entity;
 import java.sql.SQLException;
 
 import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import test.com.tunisgtug.android.cuisinetunisienne.dao.TestHelper;
 
-import com.j256.ormlite.dao.DaoManager;
-import com.j256.ormlite.dao.RuntimeExceptionDao;
-import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
-import com.j256.ormlite.spring.TableCreator;
-import com.j256.ormlite.support.ConnectionSource;
-import com.j256.ormlite.table.TableUtils;
-import com.tunisgtug.android.cuisinetunisienne.as.PlatAS;
 import com.tunisgtug.android.cuisinetunisienne.dao.entity.Ingredient;
 import com.tunisgtug.android.cuisinetunisienne.dao.entity.Plat;
-import com.tunisgtug.android.cuisinetunisienne.service.common.VariableHolder;
 
 
 /**
